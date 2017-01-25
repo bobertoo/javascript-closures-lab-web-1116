c723e011483a14554fc5dc5afaf55772dae0bebb
Vinny Martinez <vinny@vinnymartinez.com>
2017-01-25 15:45:09 -0500

    sharkWithFrickinCannon

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+function bumpCounter(){
+  var bumps = 0
+
+  function addBump(){
+    bumps++
+  }
+
+  function getBumps(){
+    return bumps
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animal) {
+
+  return function(deadlyDevice) {
+    return {
+      animalType: animal,
+      deadlyDevice: deadlyDevice
+    }
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')
